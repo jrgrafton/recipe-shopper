e2c2c271606f345934258db511de99fb03d51ca6
james <james@6dc030ff-0281-0410-8a41-8174dcc5abea>
2010-05-20 22:36:18 +0000

    ++Added SQLLite DB ++Added data access manager classes
    git-svn-id: http://svn.jamesgrafton.com/recipe-shopper/RecipeShopper/trunk@65 6dc030ff-0281-0410-8a41-8174dcc5abea

---
+++ Classes/APIRequestManager.h
@@ -0,0 +1,16 @@
+//
+//  RequestManager.h
+//  RecipeShopper
+//
+//  Created by James Grafton on 5/20/10.
+//  Copyright 2010 Asset Enhancing Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface APIRequestManager : NSObject {
+
+}
+
+@end
---
+++ Classes/APIRequestManager.m
@@ -0,0 +1,14 @@
+//
+//  RequestManager.m
+//  RecipeShopper
+//
+//  Created by James Grafton on 5/20/10.
+//  Copyright 2010 Asset Enhancing Technologies. All rights reserved.
+//
+
+#import "RequestManager.h"
+
+
+@implementation APIRequestManager
+
+@end
