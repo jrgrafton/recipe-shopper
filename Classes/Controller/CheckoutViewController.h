1bfa34bdd20b104d1a6dbd683756fa7917681c1b
sbarnett <sbarnett@6dc030ff-0281-0410-8a41-8174dcc5abea>
2010-10-18 10:28:36 +0000

    Added CheckoutXXX files
    git-svn-id: http://svn.jamesgrafton.com/recipe-shopper/TescoChef/trunk@147 6dc030ff-0281-0410-8a41-8174dcc5abea

---
+++ Classes/Controller/CheckoutViewController.h
@@ -0,0 +1,24 @@
+//
+//  CheckoutViewController.h
+//  RecipeShopper
+//
+//  Created by Simon Barnett on 13/09/2010.
+//  Copyright 2010 Assentec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DeliverySlotsViewController.h"
+
+@interface CheckoutViewController : UITableViewController <UITabBarControllerDelegate> {
+	IBOutlet UITableView *basketView;
+@private BOOL waitingForAPI;
+}
+
+@property (nonatomic, retain) DeliverySlotsViewController *deliverySlotsViewController;
+@property (nonatomic, retain) NSString *basketPrice;
+@property (nonatomic, retain) NSString *basketSavings;
+
+- (IBAction)transitionToDeliverySlotView:(id)sender;
+- (void)updateBasketDetails;
+
+@end
